60862a9f0f1786b5c334725f3d281dc17fd083bb
viraj3175 <202301008@dau.ac.in>
2026-05-30 16:54:37 +0530

    building cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a ml model which analyses all the yt video comments',
+    author='viraj krishna',
+    license='',
+)
